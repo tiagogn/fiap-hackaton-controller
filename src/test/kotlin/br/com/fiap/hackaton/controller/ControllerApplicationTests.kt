de05b45fdc083942a91614ef9f54ea3db842fa61
Tiago Gonçalves <tiagogn@gmail.com>
2025-03-15 20:04:27 -0300

    feat: desativando o sprinboottest por enquanto

--- src/test/kotlin/br/com/fiap/hackaton/controller/ControllerApplicationTests.kt
+++ src/test/kotlin/br/com/fiap/hackaton/controller/ControllerApplicationTests.kt
@@ -3,7 +3,6 @@ package br.com.fiap.hackaton.controller
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ControllerApplicationTests {
 
 	@Test
