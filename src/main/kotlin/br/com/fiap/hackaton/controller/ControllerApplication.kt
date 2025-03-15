f201c3ffc36d4db2879157007a5cab04975c1d7b
Tiago Gonçalves <tiagogn@gmail.com>
2025-03-15 14:51:30 -0300

    Primeiro commit do projeto

---
+++ src/main/kotlin/br/com/fiap/hackaton/controller/ControllerApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.hackaton.controller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControllerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControllerApplication>(*args)
+}
